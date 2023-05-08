f62e818c4542029966109f616ed090e30de0bd33
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-08 18:58:16 -0500

    Add route to product-routes file

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,6 +7,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({ 
+    raw: true 
+  }).then((data) => {
+    res.json(data);
+  })
 });
 
 // get one product
