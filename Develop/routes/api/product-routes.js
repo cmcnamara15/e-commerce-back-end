f494f0470f919afc17a40ff132a7dcf5a3b72949
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-08 21:30:05 -0500

    Add delete route in product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -101,6 +101,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      res.json(data)
+    })
+    .catch((err) => {
+      res.json(err)
+    });
 });
 
 module.exports = router;
