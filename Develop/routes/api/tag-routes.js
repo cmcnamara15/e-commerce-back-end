86cac0e76c7279fe9e0d6cce20d332af4cc7663d
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-08 21:34:49 -0500

    Add routes to tag-routes.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,15 +6,24 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    raw: true
+  }).then((data) => {
+    res.send(data);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id).then((data) => {
+    res.json(data);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.update
 });
 
 router.put('/:id', (req, res) => {
