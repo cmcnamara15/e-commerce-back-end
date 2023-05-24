b525aabc63a85cc9d2053f5bbef2faab6365dd31
Chris McNamara <cmcnamara1991@icloud.com>
2023-05-23 19:52:19 -0500

    Continue work on routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -42,10 +42,10 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+    Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length && req.body.tagsIds) {
+      if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
